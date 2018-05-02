21149b5b7895b81f90a51fd347e9915863f86325
Roman Zimine <roman@zimine.ca>
2018-05-02 05:19:22 -0400

    Up the springfox version and remove bean name spam.

--- src/main/java/com/turbonomic/demo/spring/Application.java
+++ src/main/java/com/turbonomic/demo/spring/Application.java
@@ -31,14 +31,6 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            final String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (final String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
             synchronized (serverLock) {
                 System.out.println("Starting gRPC server...");
                 grpcServer = ServerBuilder.forPort(9001)
